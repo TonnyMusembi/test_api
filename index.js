23e6305ec0193f4219a178b7152bc478537e672c
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-17 13:12:25 +0300

    ad

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+// const cors = require("cors");
+
 
 const app = express();
 
 
+
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`);
+app.listen(3001, () => {
+    console.log(`Server Started at ${3001}`);
 });
